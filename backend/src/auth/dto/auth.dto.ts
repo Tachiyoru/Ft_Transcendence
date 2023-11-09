98143e5caf4b3ca496297c4e961dce597c36394a
Shanley LEON <sleon@e1r10p1.clusters.42paris.fr>
2023-11-09 17:03:53 +0100

    up

--- backend/src/auth/dto/auth.dto.ts
+++ backend/src/auth/dto/auth.dto.ts
@@ -2,12 +2,7 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthDto {
   @IsNotEmpty()
-<<<<<<< HEAD
   @IsEmail()
-=======
-  @IsString()
-  @IsAlpha()
->>>>>>> Backend
   email: string;
 
   @IsNotEmpty()
