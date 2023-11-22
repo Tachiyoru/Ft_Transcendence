3e8a6cf367b1acea57c639068a1d724e5d62fa06
clemcheyrou <clemcheyrou@gmail.com>
2023-11-22 19:35:52 +0100

    [ADD] color

--- frontend/src/pages/_root/Dashboard.tsx
+++ frontend/src/pages/_root/Dashboard.tsx
@@ -9,40 +9,42 @@ const Dashboard = () => {
   return (
     <MainLayout currentPage={currentPage}>
         <div className="flex-1 md:flex flex-row">
+
           {/*leftSideBar*/}
-          <div className="grid grid-rows-[auto,1fr,auto] bg-purple-950  p-4 px-4 text-gray-400 rounded-l-lg">
-            
-            {/*User*/}
-            <div className="flex mt-4">
-              <div className="bg-purple-700	rounded-full p-2">
-                <FaUser className="w-[66px] h-[66px] p-3 text-purple-500	"/>
-              </div>
-              <div className="pl-4 pt-4">
-                <p className="text-xs text-purple-300">Member sinced 20/12/23</p>
-                <p className="text-sm font-semibold text-purple-500">ClemCheyrou</p>
-                <p className="mt-1 text-white text-xs font-medium text-purple-200"><span className="bg-purple-400 py-[0.15rem] px-[0.4rem] rounded">Legend</span></p>
-              </div>
-            </div>
+          <div className="md:rounded-l-lg bg-purple-950 p-4 px-4 text-gray-400 grid grid-rows-[auto,1fr,auto]">
 
-            {/*Stats*/}
-            <div className="flex flex-col justify-end mb-2">
-              <div className="bg-white dark:text-gray-200 gap-2 mt-2 h-24 dark:bg-secondary-dark-bg w-full px-2 py-2 rounded-md ">
-                <p className="text-sm text-purple-500">Total</p>
+              {/*User*/}
+              <div className="flex mt-4 mb-10">
+                <div className="bg-purple-700	rounded-full p-2">
+                  <FaUser className="w-[66px] h-[66px] p-3 text-purple-500	"/>
+                </div>
+                <div className="pl-4 pt-4">
+                  <p className="text-xs text-purple-300">Member sinced 20/12/23</p>
+                  <p className="text-sm font-semibold text-purple-500">ClemCheyrou</p>
+                  <p className="mt-1 text-white text-xs font-medium text-purple-200"><span className="bg-purple-400 py-[0.15rem] px-[0.4rem] rounded">Legend</span></p>
+                </div>
               </div>
-              <div className="flex gap-4 mt-4 flex-row">
-                  <div className="flex bg-white dark:text-gray-200 h-24 dark:bg-secondary-dark-bg w-full px-2 py-2 rounded-md ">
-                    <p className="text-sm text-purple-500">Win</p>
-                  </div>
 
-                  <div className="flex bg-white dark:text-gray-200 h-24 dark:bg-secondary-dark-bg w-full px-2 py-2 rounded-md ">
-                    <p className="text-sm text-purple-500">Loss</p>
-                  </div>
+              {/*Stats*/}
+              <div className="flex flex-col justify-end mb-2">
+                <div className="bg-white dark:text-gray-200 gap-2 mt-2 h-24 dark:bg-secondary-dark-bg w-full px-2 py-2 rounded-md ">
+                  <p className="text-sm text-purple-500">Total</p>
+                </div>
+                <div className="flex gap-4 mt-4 flex-row">
+                    <div className="flex bg-white dark:text-gray-200 h-24 dark:bg-secondary-dark-bg w-full px-2 py-2 rounded-md ">
+                      <p className="text-sm text-purple-500">Win</p>
+                    </div>
+
+                    <div className="flex bg-white dark:text-gray-200 h-24 dark:bg-secondary-dark-bg w-full px-2 py-2 rounded-md ">
+                      <p className="text-sm text-purple-500">Loss</p>
+                    </div>
+                </div>
               </div>
-            </div>
+
           </div>
 
           {/*Dashboard*/}
-          <div className="flex-1 bg-purple-950 bg-opacity-80 p-4 rounded-r-lg">
+          <div className="flex-1 bg-purple-950 bg-opacity-80 p-4 md:rounded-r-lg">
 
             <div className="flex m-2 flex-row gap-4 md:gap-6">
 
