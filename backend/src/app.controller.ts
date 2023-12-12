f0eb6611f307956b76706b54342ade55377a41d9
Shanley LEON <shanley@hotmail.fr>
2023-12-12 13:26:57 +0100

    [small fix]

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,40 +1,18 @@
-import
-{
-	Controller,
-	Get,
-	Post,
-	Req,
-	Request,
-	UploadedFiles,
-	UseGuards,
-	UseInterceptors,
-} from "@nestjs/common";
+import { Controller, Get, Post, Req, Request, UseGuards } from "@nestjs/common";
 import { AppService } from "./app.service";
-import { FilesInterceptor } from "@nestjs/platform-express";
-import { Express } from "express";
 import { TokenGuard } from "./auth/guard/token.guard";
 
 @Controller()
-export class AppController
-{
-	constructor(private readonly appService: AppService) {}
+export class AppController {
+  constructor(private readonly appService: AppService) {}
 
-	@UseGuards(TokenGuard)
-	@Get("hello")
-	async getUserData(@Request() req: any)
-	{
-		const user = req.user;
-		console.log("print user", user);
-	}
-	getHello(@Req() req: any): string
-	{
-		return this.appService.getHello();
-	}
-
-	@Post()
-	@UseInterceptors(FilesInterceptor("image"))
-	uploadFile(@UploadedFiles() file: Express.Multer.File)
-	{
-		console.log(file);
-	}
+  @UseGuards(TokenGuard)
+  @Get("hello")
+  async getUserData(@Request() req: any) {
+    const user = req.user;
+    console.log("print user", user);
+  }
+  getHello(@Req() req: any): string {
+    return this.appService.getHello();
+  }
 }
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,34 +1,46 @@
-import { Body, Controller, Get, Patch, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+} from "@nestjs/common";
 import { User } from "@prisma/client";
 import { GetUser } from "../auth/decorator";
 import { EditUserDto } from "./dto";
 import { UserService } from "./user.service";
 import { TokenGuard } from "../auth/guard/token.guard";
+import { FilesInterceptor } from "@nestjs/platform-express";
 
 @Controller("users")
-export class UserController
-{
-	constructor(private userService: UserService) {}
+export class UserController {
+  constructor(private userService: UserService) {}
 
+  @UseGuards(TokenGuard)
+  @Get("me")
+  getMe(@GetUser() user: User) {
+    console.log(user);
+    return user;
+  }
 
-	@UseGuards(TokenGuard)
-	@Get("me")
-	getMe(@GetUser() user: User)
-	{
-		console.log(user)
-		return user;
-	}
+  @Get("all")
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
 
-	@Get("all")
-	getAllUsers() {
-		return this.userService.getAllUsers();
-	}
+  @Post("addAvatar")
+  @UseInterceptors(FilesInterceptor("image"))
+  uploadFile(@UploadedFiles() file: Express.Multer.File) {
+    console.log(file);
+  }
 
-	@UseGuards(TokenGuard)
-	@Patch("edit")
-	editUser(@GetUser("id") userId: number, @Body() dto: EditUserDto)
-	{
-		console.log("userId : ", userId)
-		return (this.userService.editUser(userId, dto));
-	}
+  @UseGuards(TokenGuard)
+  @Patch("edit")
+  editUser(@GetUser("id") userId: number, @Body() dto: EditUserDto) {
+    console.log("userId : ", userId);
+    return this.userService.editUser(userId, dto);
+  }
 }
