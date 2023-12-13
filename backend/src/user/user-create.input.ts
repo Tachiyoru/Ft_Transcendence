60ceade5e46393a542c49425cf8de51314851065
Manuel SHARIFI <manuel.sharifi@gmail.com>
2023-12-13 22:14:49 +0100

    [MERGE]

--- backend/src/user/user-create.input.ts
+++ backend/src/user/user-create.input.ts
@@ -1,19 +1,23 @@
 import { Role } from "@prisma/client";
+import { Stats } from "fs";
 
-export interface UserCreateInput {
-  username: string;
-  avatar?: string;
-  tittle?: string;
-  email: string;
-  hash: string;
-  role: Role;
+export interface UserCreateInput
+{
+	username: string;
+	avatar?: string;
+	tittle?: string;
+	email: string;
+	hash: string;
+	role: Role;
+	stats: statsCreateInput;
 }
 
-export interface statsCreateInput {
+export interface statsCreateInput
+{
 	lvl?: number;
 	exp?: number;
 	partyPlayed?: number;
 	partyWon?: number;
 	partyLost?: number;
 	history?: [string]
-  };
+};
