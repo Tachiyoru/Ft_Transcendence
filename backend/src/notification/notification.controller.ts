fedc6dcb6ea6a2db3b0c0f53b41db97d33f97aca
Manuel SHARIFI <manuel.sharifi@gmail.com>
2023-12-15 13:59:25 +0100

    Merge main

--- backend/src/notification/notification.controller.ts
+++ backend/src/notification/notification.controller.ts
@@ -1,13 +1,14 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-  UseGuards,
+import
+{
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	ParseIntPipe,
+	Patch,
+	Post,
+	UseGuards,
 } from "@nestjs/common";
 import { TokenGuard } from "src/auth/guard";
 import { NotificationService } from "./notification.service";
@@ -17,59 +18,66 @@ import { CreateNotificationDto } from "./dto/create-notification.dto";
 
 @Controller("notification")
 @UseGuards(TokenGuard)
-export class NotificationController {
-  constructor(private notificationService: NotificationService) {}
+export class NotificationController
+{
+	constructor(private notificationService: NotificationService) {}
 
-  @Get("mine")
-  async getMyNotifications(@GetUser() user: User): Promise<Notification[]> {
-    return this.notificationService.getMyNotifications(user);
-  }
+	@Get("mine")
+	async getMyNotifications(@GetUser() user: User): Promise<Notification[]>
+	{
+		return this.notificationService.getMyNotifications(user);
+	}
 
-  // test only, delete after
-  @Get("all")
-  async getAllNotifications(): Promise<Notification[]> {
-    return this.notificationService.getAllNotifications();
-  }
+	// test only, delete after
+	@Get("all")
+	async getAllNotifications(): Promise<Notification[]>
+	{
+		return this.notificationService.getAllNotifications();
+	}
 
-  @Post("add/:id")
-  async addNotificationByUserId(
-    @Param("id", ParseIntPipe) userId: number,
-    @Body() notificationDto: CreateNotificationDto,
-    @Body("type", ParseIntPipe) notifType: number
-  ): Promise<Notification> {
-    return this.notificationService.addNotificationByUserId(
-      userId,
-      notificationDto,
-      notifType
-    );
-  }
+	@Post("add/:id")
+	async addNotificationByUserId(
+		@Param("id", ParseIntPipe) userId: number,
+		@Body() notificationDto: CreateNotificationDto,
+		@Body("type", ParseIntPipe) notifType: number
+	): Promise<Notification>
+	{
+		return this.notificationService.addNotificationByUserId(
+			userId,
+			notificationDto,
+			notifType
+		);
+	}
 
-  @Delete("delete/:userId/:id")
-  async deleteNotificationById(
-    @Param("userId", ParseIntPipe) userId: number,
-    @Param("id", ParseIntPipe) notificationId: number
-  ) {
-    return this.notificationService.deleteNotificationById(
-      userId,
-      notificationId
-    );
-  }
+	@Delete("delete/:userId/:id")
+	async deleteNotificationById(
+		@Param("userId", ParseIntPipe) userId: number,
+		@Param("id", ParseIntPipe) notificationId: number
+	)
+	{
+		return this.notificationService.deleteNotificationById(
+			userId,
+			notificationId
+		);
+	}
 
-  @Patch("read/:userId/:id")
-  async setNotificationReadById(
-    @Param("userId", ParseIntPipe) userId: number,
-    @Param("id", ParseIntPipe) notificationId: number
-  ): Promise<Notification> {
-    return this.notificationService.setNotificationReadById(
-      userId,
-      notificationId
-    );
-  }
+	@Patch("read/:userId/:id")
+	async setNotificationReadById(
+		@Param("userId", ParseIntPipe) userId: number,
+		@Param("id", ParseIntPipe) notificationId: number
+	): Promise<Notification>
+	{
+		return this.notificationService.setNotificationReadById(
+			userId,
+			notificationId
+		);
+	}
 
-  @Get(":id")
-  async getNotificationsById(
-    @Param("id", ParseIntPipe) userId: number
-  ): Promise<Notification[]> {
-    return this.notificationService.getNotificationsById(userId);
-  }
+	@Get(":id")
+	async getNotificationsById(
+		@Param("id", ParseIntPipe) userId: number
+	): Promise<Notification[]>
+	{
+		return this.notificationService.getNotificationsById(userId);
+	}
 }
