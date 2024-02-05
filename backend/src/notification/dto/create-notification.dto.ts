ab9e2487190f85a53414a86118fa2965412f6ff8
Shanley LEON <shanley@hotmail.fr>
2024-02-05 18:25:46 +0100

    [hotfix]

--- backend/src/notification/dto/create-notification.dto.ts
+++ backend/src/notification/dto/create-notification.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmpty, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateNotificationDto {
   @IsOptional()
